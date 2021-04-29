f5bb3951686841850022e974c78b5af2dc56a1fe
Sergio884 <edilberto.sergio.valle.ortiz@gmail.com>
2021-04-28 22:51:12 -0500

    Funcional

--- POV.asm
+++ POV.asm
@@ -1,13 +1,13 @@
 .include"m8535def.inc"
-.def data =r16
-ldi data,low(RAMEND)
-out spl,data
-ldi data,high(RAMEND)
-out sph,data
-ser data
-out ddra,data
-out portb, data
-out ddrc,data
+.def dat =r16
+ldi dat,low(RAMEND)
+out spl,dat
+ldi dat,high(RAMEND)
+out sph,dat
+ser dat
+out ddra,dat
+out portb, dat
+out ddrc,dat
 
 	ldi r20,$3F
 	ldi r21,$06
@@ -21,53 +21,41 @@ out ddrc,data
 	ldi r29,$6f
 	clr zh
 
-cambio:
-	
-	in data,pinb;Asignar en data lo que hay en el Puerto B
-	andi data,$0F;enmascarar el nible alto del data
-
-	rcall ddata ;llamamos a ddata
-	/*Con estas dos lineas asignamos un 1 en el puerto C y un 0*/
+otro:
+	in dat,pinb
+	andi dat,$0F
+	rcall ddat
 	sbi portc,1 
 	cbi portc,0
-	/*Mostramos en el puerto A lo que hay en data*/
-	out porta,data
-	/*llamamos a nuestro delay*/
-	rcall retraso
-	/*asignamos en el puerto A, lo que hay en la bandera Zh*/
+	out porta,dat
+	rcall delay
 	out porta,zh
 
-	in data,pinb;Asignar en data lo que hay en el Puerto B
-	andi data,$F0;enmascarar el nible bajo del data
-	swap data ; hacemos un swap para pasar el nible alto al bajo
-	
-	rcall ddata;;llamamos a ddata
-	/*Con estas dos lineas asignamos un 0 en el puerto C y un 1*/
+	in dat,pinb
+	andi dat,$F0
+	swap dat
+	rcall ddat
 	sbi portc,0
 	cbi portc,1
-	/*Mostramos en el puerto A lo que hay en data*/
-	out porta,data
-	/*llamamos a nuestro delay*/
-	rcall retraso
-	/*asignamos en el puerto A, lo que hay en la bandera Zh*/
+	out porta,dat
+	rcall delay
 	out porta,zh
-
-	rjmp cambio;regresamos al inicio de cambio
-ddata:
-	ldi zl,20;asignamos en zl un 20
-	add zl,data;Sumamos lo que hay en data
-	ld data,z ; asignamos lo que hay en la bandera z en data
-	ret
-	/*Estas funciones nos ayudaran a agregar un delay de 996 ciclos a 1Mhz*/
-retraso:
+	rjmp otro
+ddat:
+	ldi zl,20
+	add zl,dat
+	ld dat,z
 	ret
 
-	ldi r18,2
-	ldi r19, 74
+delay:
+	ret
+
+	ldi r17,2
+	ldi r18, 74
 L1:
-	dec r19
-	brne L1
 	dec r18
+	brne L1
+	dec r17
 	brne L1
 	nop
 	ret
